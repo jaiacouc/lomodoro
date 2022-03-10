129ae74f71a2732a1f46401fcba6c5ed5df2591f
jaiacouc <jaiacouc@uncg.edu>
2022-03-10 12:56:15 -0500

    Added Pomodoro and break buttons

--- src/Components/Pomodoro/Pomodoro.js
+++ src/Components/Pomodoro/Pomodoro.js
@@ -1,11 +1,13 @@
 import Work from "./Work";
 import Break from "./Break";
 import Card from "../UI/Card";
+import SelectPomodoro from "./SelectPomodoro";
 import styles from "./Pomodoro.module.css";
 
 function Pomodoro(props) {
   return (
     <Card className={styles.pomodoro}>
+      <SelectPomodoro></SelectPomodoro>
       <Work timeMinutes={props.workMinutes}></Work>
       <Break timeMinutes={props.breakMinutes}></Break>
     </Card>
---
+++ src/Components/Pomodoro/SelectPomodoro.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const SelectPomodoro = (props) => {
+  return (
+    <div>
+      <button>Pomodoro</button>
+      <button>Break</button>
+    </div>
+  );
+};
+
+export default SelectPomodoro;
